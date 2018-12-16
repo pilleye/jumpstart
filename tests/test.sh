3c625e43ce2596bc2613906a0a674bd8bae1dba0
Aditya Pillai <github@pillai.io>
2018-12-15 19:28:29 -0600

    Jumpstart documentation

--- tests/test.sh
+++
@@ -1,7 +0,0 @@
-set -e 
-
-echo; echo "Running test installation..."
-source .venv/bin/activate
-
-# Run ansible playbook
-ansible-playbook -i inventories/inventory playbooks/main.yml --extra-vars "@./global_vars/test.yml" --extra-vars "@./global_vars/vars.yml"
