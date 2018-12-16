7a80ea73ba356dd32105f55c1067895d3c292950
Aditya Pillai <github@pillai.io>
2018-12-15 18:26:50 -0600

    Refactor

--- tests/test.sh
+++ tests/test.sh
@@ -4,4 +4,4 @@ echo; echo "Running test installation..."
 source .venv/bin/activate
 
 # Run ansible playbook
-ansible-playbook -i inventories/inventory playbooks/main.yml --extra-vars "@./global_vars/test.yml"
+ansible-playbook -i inventories/inventory playbooks/main.yml --extra-vars "@./global_vars/test.yml" --extra-vars "@./global_vars/vars.yml"
