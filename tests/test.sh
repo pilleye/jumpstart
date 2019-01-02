03d1896749262e8f6cfd9d0fbc1e5f502d5ddf2c
Aditya Pillai <github@pillai.io>
2019-01-02 01:25:42 -0600

    Fix discrepancy between actual usage and CI

--- tests/test.sh
+++ tests/test.sh
@@ -4,4 +4,4 @@ echo; echo "Running test installation..."
 source .venv/bin/activate
 
 # Run ansible playbook
-ansible-playbook -i inventories/inventory playbooks/main.yml --extra-vars "@./global_vars/test.yml" --extra-vars "@./global_vars/no_vars.yml" --extra-vars "$TEST_VAR"
+ansible-playbook -i inventories/inventory playbooks/main.yml --extra-vars "@./global_vars/no_vars.yml" --extra-vars "@./global_vars/test.yml"  --extra-vars "$TEST_VAR"
