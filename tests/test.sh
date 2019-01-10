8e7d92ba09b9a380dc6b504c0a5762806bffa7df
Aditya Pillai <github@pillai.io>
2019-01-09 18:28:30 -0600

    Update and upgrade before calling playbook so that Ansible doesn't time out

--- tests/test.sh
+++ tests/test.sh
@@ -3,5 +3,7 @@ set -e
 echo; echo "Running test installation..."
 source .venv/bin/activate
 
+brew update && brew upgrade
+
 # Run ansible playbook
 ansible-playbook -i inventories/inventory playbooks/main.yml --extra-vars "@./global_vars/no_vars.yml" --extra-vars "@./global_vars/test.yml"  --extra-vars "$TEST_VAR"
