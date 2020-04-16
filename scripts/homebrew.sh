dbee6edfbe034f0279044158805fb376bcbcc7ee
Aditya Pillai <github@pillai.io>
2020-04-16 13:58:38 -0500

    Fix command used for installing homebrew

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -9,7 +9,7 @@ echo "Checking if Homebrew is already installed...";
 # Credit: https://gist.github.com/codeinthehole/26b37efa67041e1307db
 if test ! $(which brew); then
   echo "Installing Homebrew...";
-  yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else
   echo "Homebrew is already installed...";
 fi
